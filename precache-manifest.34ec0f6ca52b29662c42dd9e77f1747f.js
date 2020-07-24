fc2b5d4d3a56de68b982255fb893f33c41ec2291
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-24 03:06:52 +0000

    Deploy gbdu/magici to github.com/gbdu/magici.git:gh-pages

---
+++ precache-manifest.34ec0f6ca52b29662c42dd9e77f1747f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "377047827a1e51e2bdbee72d22b19613",
+    "url": "/magici/index.html"
+  },
+  {
+    "revision": "05e77f5ff97098e4dafa",
+    "url": "/magici/static/css/main.d18dee0f.chunk.css"
+  },
+  {
+    "revision": "a580f9687f5f50203f05",
+    "url": "/magici/static/js/2.f545bfbf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/magici/static/js/2.f545bfbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e77f5ff97098e4dafa",
+    "url": "/magici/static/js/main.1bad334b.chunk.js"
+  },
+  {
+    "revision": "ec427a230c4daafb706b",
+    "url": "/magici/static/js/runtime-main.9d53fa37.js"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/magici/static/media/logo.8dad8028.svg"
+  }
+]);
